200700928584378fdac06f9b160eeaf1df33e295
Ethan Ruhl <epruhl102@gmail.com>
2021-04-13 14:07:13 -0400

    Added Grate interaction scripts

---
+++ WaterGame/Assets/Scripts/ObjectScripts/TriggerEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public delegate void Trigger(bool enter, Collider other);
+    public Trigger triggerEvent;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent?.Invoke(true, other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerEvent?.Invoke(false, other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Limit to player interaction
+        if(other.CompareTag("Player"))
+        {
+            triggerEvent?.Invoke(true, other);
+        }
+       
+    }
+}
