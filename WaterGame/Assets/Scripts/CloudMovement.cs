e41dac04061bd936726f30ca8adacabe0cdef99a
Ethan Ruhl <epruhl102@gmail.com>
2021-04-13 13:02:03 -0400

    Added playerStateChange

---
+++ WaterGame/Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    float startYPos;
+    public float magnitude;
+    public float speed;
+
+    float timeElapsed;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        startYPos = transform.localPosition.y;
+    }
+    void Update()
+    {
+
+        //Move cloud model up and down (in local space) to give it "life"
+        timeElapsed += Time.deltaTime * speed;
+        transform.localPosition = new Vector3(0, startYPos + (Mathf.Sin(timeElapsed) * magnitude), 0);
+    }
+}
---
+++ WaterGame/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public enum PlayerMatterState { DROP,CLOUD,ICE }
+public class PlayerState : MonoBehaviour
+{
+    //Can be accessed from other scripts
+    public static PlayerMatterState currentPlayerState = PlayerMatterState.DROP;
+
+
+    //References
+    public GameObject dropModel;
+    public GameObject cloudModel;
+    public GameObject iceModel;
+    public ParticleSystem changeParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdatePlayerModel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Drop
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if(currentPlayerState != PlayerMatterState.DROP)
+            {
+                currentPlayerState = PlayerMatterState.DROP;
+                UpdatePlayerModel();
+            }
+          
+        }
+        //Ice
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (currentPlayerState != PlayerMatterState.CLOUD)
+            {
+                currentPlayerState = PlayerMatterState.CLOUD;
+                UpdatePlayerModel();
+            }
+          
+        }
+        //Cloud
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (currentPlayerState != PlayerMatterState.ICE)
+            {
+                currentPlayerState = PlayerMatterState.ICE;
+                UpdatePlayerModel();
+            }
+        }
+
+    }
+
+
+    /// <summary>
+    /// Update the player model to match the current state
+    /// </summary>
+    private void UpdatePlayerModel()
+    {
+        changeParticle.Play();
+
+        switch (currentPlayerState)
+        {
+            case PlayerMatterState.CLOUD:
+                cloudModel.SetActive(true);
+                iceModel.SetActive(false);
+                dropModel.SetActive(false);
+                break;
+
+            case PlayerMatterState.ICE:
+                iceModel.SetActive(true);
+                cloudModel.SetActive(false);
+                dropModel.SetActive(false);
+
+                break;
+
+            case PlayerMatterState.DROP:
+                dropModel.SetActive(true);
+                iceModel.SetActive(false);
+                cloudModel.SetActive(false);
+
+                break;
+
+            default:
+                return;
+        }
+
+    }
+}
