b353f3584115aac9afbb75a3c83a1d3ad618ce0b
Spiritdon <dj5235@g.rit.edu>
2021-04-20 03:01:52 -0400

    Attempted Fix of Trampoline

--- WaterGame/Assets/trampInteraction.cs
+++ WaterGame/Assets/trampInteraction.cs
@@ -6,27 +6,44 @@ public class trampInteraction : MonoBehaviour
 {
     private float tramForce;
     private TriggerEvent triggerEvent;
+    public PlayerMovement movement;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        tramForce = 10;
+        tramForce = 25;
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*if (movement.IsTrampHit())
+        {
+            Rigidbody playerRB = player.GetComponent<Rigidbody>();
+            tramForce = 25;
+            print(tramForce);
+            playerRB.AddForce(Vector3.up * tramForce, ForceMode.Impulse);
+        }*/
     }
-    private void OnCollisionEnter(Collision collision)
+    /*private void OnCollisionEnter(Collision collision)
     {
         if (collision.other.CompareTag("Player") && PlayerState.currentPlayerState == PlayerMatterState.ICE)
         {
             Rigidbody playerRB = collision.other.GetComponent<Rigidbody>();
+            //tramForce += Mathf.Abs(playerRB.velocity.y);
             if (tramForce<25)
             {
-                tramForce += Mathf.Abs(playerRB.velocity.y);
+
             }
-            //print(tramForce);
+            else if(tramForce >= 25)
+            {
+                tramForce = 25;
+            }
+            tramForce = 25;
+            print(tramForce);
             playerRB.AddForce(Vector3.up * tramForce, ForceMode.Impulse);
         }
-    }
+        
+
+    }*/
 }
