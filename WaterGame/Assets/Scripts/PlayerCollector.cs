1b37d7e0b7aa0990f782f1beb81906a1f67f5569
Ethan Ruhl <epruhl102@gmail.com>
2021-04-28 14:09:58 -0400

    Added flavor/sugar collection

---
+++ WaterGame/Assets/Scripts/PlayerCollector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollector : MonoBehaviour
+{
+    //Progress manager
+    ProgressManager pm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = ProgressManager.Instance;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Flavor Collision
+        if (other.CompareTag("FlavorCOL"))
+        {
+            FlavorPickup flav = other.gameObject.GetComponent<FlavorPickup>();
+
+            if (flav != null)
+            {
+                pm.CollectedFlavor(flav.flavor);
+                Destroy(other.gameObject);
+            }
+        }
+
+        //Sugar collision
+        else if (other.CompareTag("SugarCOL"))
+        {
+            pm.sugarCollected++;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ WaterGame/Assets/Scripts/ScriptableObjects/ProgressManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Progress Manager")]
+public class ProgressManager : ScriptableObject
+{
+    public List<string> flavors;
+    private Dictionary<string, bool> collectedFlavors = new Dictionary<string, bool>();
+    public int sugarCollected = 0;
+
+    private static ProgressManager instance;
+
+    public static ProgressManager Instance { get => instance; }
+
+    private void OnEnable()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        ReInitializeDict();
+    }
+
+    public void CollectedFlavor(string flavor)
+    {
+        if(collectedFlavors.ContainsKey(flavor))
+        {
+            collectedFlavors[flavor] = true;
+            Debug.Log("Collected " + flavor);
+        }
+    }
+
+
+    /// <summary>
+    /// Has the player collected this flavor yet?
+    /// </summary>
+    /// <param name="flavor">String name of the flavor</param>
+    /// <returns>TRUE if player has collected the flavor</returns>
+    public bool HasFlavor(string flavor)
+    {
+        if (collectedFlavors.ContainsKey(flavor))
+        {
+            if(collectedFlavors[flavor])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private void ReInitializeDict()
+    {
+        collectedFlavors.Clear();
+        for (int i = 0; i < flavors.Count; i++)
+        {
+            collectedFlavors.Add(flavors[i], false);
+        }
+    }
+}
