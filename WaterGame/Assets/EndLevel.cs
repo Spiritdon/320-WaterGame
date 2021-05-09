0c69cb1f5ba5bea5389dba5ad695999ff6f1eb46
heartsail <54554659+heartsail@users.noreply.github.com>
2021-05-09 18:23:35 -0400

    Cutscene and Scene Swap Trigger added

---
+++ WaterGame/Assets/EndLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+
+    public GameObject cutscene;
+    public int playtime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cutscene.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            cutscene.SetActive(true);
+            Destroy(cutscene, playtime);
+        }
+    }
+}
