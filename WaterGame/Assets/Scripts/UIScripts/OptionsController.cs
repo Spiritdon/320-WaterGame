424611bbff747e2085436f06e3394b38d6a54192
Ethan Ruhl <epruhl102@gmail.com>
2021-05-12 04:20:53 -0400

    player dash fix, volume settings

---
+++ WaterGame/Assets/Scripts/UIScripts/OptionsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsController : MonoBehaviour
+{
+
+
+    public AudioMixer masterMixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ChangeMasterVolume(float vol)
+    {
+        float finalVol = Mathf.Lerp(-50, 10f, vol);
+        masterMixer.SetFloat("ooga", finalVol);
+    }
+
+    public void ChangeMusicVolume(float vol)
+    {
+        float finalVol = Mathf.Lerp(-50, 10f, vol);
+        masterMixer.SetFloat("BG_Music", finalVol);
+    }
+}
