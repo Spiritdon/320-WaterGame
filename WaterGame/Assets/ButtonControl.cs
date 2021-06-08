4eaa4cdc43416e961dc51d97b676cd8526ad1432
Spiritdon <dj5235@g.rit.edu>
2021-06-08 11:22:08 -0400

    Attempt to set up the options menu

--- WaterGame/Assets/ButtonControl.cs
+++ WaterGame/Assets/ButtonControl.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonControl : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject optionPanel;
+    public GameObject pausePanel;
 
     public void OpenPanel()
     {
@@ -27,6 +30,24 @@ public class ButtonControl : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void OptionsPanelToggle()
+    {
+        optionPanel.SetActive(!optionPanel.activeSelf);
+        if (optionPanel.activeSelf == true)
+        {
+            pausePanel.SetActive(false);
+            pausePanel.GetComponent<Image>().raycastTarget = false;
+        }
+        else
+        {
+            pausePanel.SetActive(true);
+            pausePanel.GetComponent<Image>().raycastTarget = true;
+        }
+    }
+    public void CloseFromOptions()
+    {
+        optionPanel.SetActive(false);
     }
     public void ChangeColor()
     {
